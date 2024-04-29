9f004f8ec7795e432311e843548f693967ee160a
molu <msaezcardenas@gmail.com>
2024-04-29 16:11:58 -0400

    validacion del formulario

--- src/components/Auth/RegisterForm/RegisterForm.js
+++ src/components/Auth/RegisterForm/RegisterForm.js
@@ -1,23 +1,58 @@
 import { Form } from 'semantic-ui-react';
+import { useFormik } from 'formik';
+import { initialValues, validationSchema } from './RegisterForm.form';
 
 export function RegisterForm() {
+  const formik = useFormik({
+    initialValues: initialValues(),
+    validationSchema: validationSchema(),
+    validateOnChange: false,
+    onSubmit: () => {
+      console.log('formulario enviado');
+    },
+  });
+
   return (
-    <Form>
+    <Form onSubmit={formik.handleSubmit}>
       <Form.Group widths='equal'>
-        <Form.Input name='email' type='text' placeholder='Correo electronico' />
+        <Form.Input
+          name='email'
+          type='text'
+          placeholder='Correo electronico'
+          value={formik.values.email}
+          onChange={formik.handleChange}
+          error={formik.errors.email}
+        />
         <Form.Input
           name='username'
           type='text'
           placeholder='Nombre de usuario'
+          value={formik.values.username}
+          onChange={formik.handleChange}
+          error={formik.errors.username}
         />
       </Form.Group>
 
       <Form.Group widths='equal'>
-        <Form.Input name='name' type='text' placeholder='Nombre y Apellidos' />
-        <Form.Input name='password' type='password' placeholder='Contraseña' />
+        <Form.Input
+          name='name'
+          type='text'
+          placeholder='Nombre y Apellidos'
+          value={formik.values.name}
+          onChange={formik.handleChange}
+          error={formik.errors.name}
+        />
+        <Form.Input
+          name='password'
+          type='password'
+          placeholder='Contraseña'
+          value={formik.values.password}
+          onChange={formik.handleChange}
+          error={formik.errors.password}
+        />
       </Form.Group>
 
-      <Form.Button type='submit' fluid>
+      <Form.Button type='submit' fluid loading={formik.isSubmitting}>
         Registrarse
       </Form.Button>
     </Form>
